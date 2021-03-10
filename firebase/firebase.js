9c86cabb9654739534644f695a4a8d9975dc9e29
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2021-03-10 22:29:51 +0100

    Added "edit profile" functionality, works with DB

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -12,11 +12,12 @@ const firebaseConfig = {
   messagingSenderId: "348584618121",
   appId: "1:348584618121:web:f7d2f439368684b465fe58",
   measurementId: "G-N3TEQW5ZQR",
-  databaseURL:"degenek-58dd9-default-rtdb"
+  databaseURL:"https://degenek-58dd9-default-rtdb.firebaseio.com/"
 };
   firebase.initializeApp(firebaseConfig);
 } catch (error) {
   console.log(error)
 }
+
 
 export default firebase;
